cf796bdc77faa9dee469e7872bee8f371ccc9c04
BEOKS <lee01042000@gmail.com>
2021-03-27 14:12:01 +0900

    inclued opencv cpp

--- receiver.cpp
+++ receiver.cpp
@@ -1,7 +1,28 @@
+#include <opencv/build/include/opencv2/opencv.hpp>
+#include <iostream>
+using namespace std;
+using namespace cv;
+
+
 int receiver(){
     //TODO 1. 인풋 스트림을 열어서
-    stream = Gobject.~~~
-    frame = stream.read()
+    // stream = Gobject.~~~
+    // frame = stream.read()
     //TODO 2. 수신 받은 걸 openCV에 쓸 수 있도록 가공
+
     //TODO 3. openCV에서 돌려서 결과출력
+    Mat frame;
+
+    while(true) {
+
+        cap.read(frame);
+
+        if(frame.empty())
+            break;
+
+        imshow("Receiver", frame);
+        if(waitKey(1) == 'r')
+            break;
+    }
+    destroyWindow("Receiver");
 }
--- sender.cpp
+++ sender.cpp
@@ -1,8 +1,8 @@
 int sender(char ip[],char port[]){
     //TODO 1. 카메라와 연결
     //TODO 2. 영상을 224x224로 리사이징
-    frame
+    // frame
     //TODO 3. 아이피와 포트를 이용 영상을 프레임당 전송
-    stream=Gobject.~~~
-    stream.write()
+    // stream=Gobject.~~~
+    // stream.write()
 }
