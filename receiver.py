def786d4b3ccd3379db0b41b393c8474d7324f21
hymi54 <hymi54@knu.ac.kr>
2021-05-14 00:09:02 +0900

    receiver.py
    Write receiver.py

---
+++ receiver.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+import numpy as np
+
+cap = cv.VideoCapture('udpsrc port=9777 ! application/x-rtp ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! appsink', cv.CAP_GSTREAMER)
+
+if not cap.isOpened():
+    print("VideoCapture not opened")
+    exit(-1)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("empty frame")
+        break
+    cv.imshow("Receiver", frame)
+    if cv.waitKey(1) == 'r':
+        break
+
+cap.release()
+cv.destroyWindow("Receiver")
+# print(cv.getBuildInformation())
