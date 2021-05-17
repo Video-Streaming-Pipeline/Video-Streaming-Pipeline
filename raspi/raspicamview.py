bcf996e66cc1cbd2902f8a8436171bcc5c96435a
hyoeun98 <f2921641@naver.com>
2021-05-17 23:37:44 +0900

    multiprocessing

--- raspi/raspicamview.py
+++ raspi/raspicamview.py
@@ -1,11 +1,12 @@
 import cv2 as cv
 import numpy as np
 import threading
+from multiprocessing import Process
 from picamera import PiCamera
  
 def sending():
     cap = cv.VideoCapture(-1, cv.CAP_V4L2)
-    out = cv.VideoWriter('appsrc ! videoconvert ! x264enc tune=zerolatency ! rtph264pay ! udpsink host=127.0.0.1 port=9777', 0, 30, (224, 224))
+    out = cv.VideoWriter('appsrc ! videoconvert ! x264enc tune=zerolatency ! rtph264pay ! udpsink host=221.164.144.187 port=9777', 0, 30, (224, 224))
     while True:
         res,frame = cap.read()
         if res:
@@ -17,7 +18,7 @@ def sending():
 
 def receive():
     print('before writer open')
-    cap = cv.VideoCapture('udpsrc port=9887 ! application/x-rtp ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! appsink', cv.CAP_GSTREAMER)
+    cap = cv.VideoCapture('udpsrc port=9888 ! application/x-rtp ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! appsink', cv.CAP_GSTREAMER)
     print('writer open')
     while True:
         res, frame = cap.read()
@@ -26,8 +27,14 @@ def receive():
             if cv.waitKey(20) == 27:
                 break
                         
-send = threading.Thread(target=sending)
-receive = threading.Thread(target=receive)
-
-send.start()
-receive.start()
+#send = threading.Thread(target=sending)
+#receive = threading.Thread(target=receive)
+#send.start()
+#receive.start()
+if __name__ == '__main__':
+    s=Process(target=sending)
+    r=Process(target=receive)
+    s.start()
+    r.start()
+    s.join()
+    r.join()
