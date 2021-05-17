daa25143520d1735c7f3a77674e0604a9d31e514
hyoeun98 <f2921641@naver.com>
2021-05-17 19:04:48 +0900

    threading

--- raspi/raspicamview.py
+++ raspi/raspicamview.py
@@ -1,20 +1,33 @@
 import cv2 as cv
 import numpy as np
+import threading
 from picamera import PiCamera
-cap = cv.VideoCapture(-1, cv.CAP_V4L2)
-out = cv.VideoWriter('appsrc ! videoconvert ! x264enc tune=zerolatency ! rtph264pay ! udpsink host=127.0.0.1 port=5000', 0, 30, (224, 224))
  
+def sending():
+    cap = cv.VideoCapture(-1, cv.CAP_V4L2)
+    out = cv.VideoWriter('appsrc ! videoconvert ! x264enc tune=zerolatency ! rtph264pay ! udpsink host=127.0.0.1 port=9777', 0, 30, (224, 224))
+    while True:
+        res,frame = cap.read()
+        if res:
+            re_frame = cv.resize(frame, (224,224), 0, 0, cv.INTER_LINEAR)
+            out.write(re_frame)
+            cv.imshow("Send img", re_frame)
+            if cv.waitKey(20) == 27:
+                break
 
-while True:
-    res, frame = cap.read()
-    if res:
-        re_frame = cv.resize(frame, (224, 224), 0, 0, cv.INTER_LINEAR)
-        out.write(re_frame)
-        cv.imshow("image", re_frame)
-        if cv.waitKey(20) == 27:
-            break
+def receive():
+    print('before writer open')
+    cap = cv.VideoCapture('udpsrc port=9887 ! application/x-rtp ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! appsink', cv.CAP_GSTREAMER)
+    print('writer open')
+    while True:
+        res, frame = cap.read()
+        if res:
+            cv.imshow("Receive img", frame)    
+            if cv.waitKey(20) == 27:
+                break
                         
+send = threading.Thread(target=sending)
+receive = threading.Thread(target=receive)
 
-cap.release()
-out.release()
-
+send.start()
+receive.start()
