9643362c2784c5f3b78f5fff54c9bdae1147b3ee
hyoeun98 <f2921641@naver.com>
2021-05-24 02:23:15 +0900

    Update raspicamview.py

---
+++ raspi/build/CMakeFiles/raspicamview.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raspicamview.dir/raspicamview.cpp.o"
+  "raspicamview"
+  "raspicamview.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raspicamview.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
